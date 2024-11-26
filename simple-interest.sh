b1b1558a7b37a2ea0eb8a2c9dcbaf1a8e801eaf0
malang08 <159497062+malang08@users.noreply.github.com>
2024-11-26 15:51:30 +0000

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+# Script pour calculer l'intérêt simple
+echo "Entrez le principal : "
+read principal
+echo "Entrez le taux d'intérêt (en %) : "
+read rate
+echo "Entrez la durée (en années) : "
+read time
+interest=$(echo "$principal * $rate * $time / 100" | bc -l)
+echo "L'intérêt simple est : $interest"
